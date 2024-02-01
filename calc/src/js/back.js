515423c0285361640033168fc812d3bfb751ccca
naleilan <leilan.n@gmail.com>
2024-02-01 21:56:07 +0300

    add blog

--- calc/src/js/back.js
+++
@@ -1,6 +0,0 @@
-const calcBtn = document.querySelector('.btn-calc');
-const container = document.querySelector('.container');
-const main = document.querySelector('.main');
-
-container.classList.add('hidden');
-main.classList.remove('hidden');
--- calc/src/js/calc.js
+++
@@ -1,6 +0,0 @@
-const calcBtn = document.querySelector('.btn-calc');
-const btnback = document.querySelector('.btn-back');
-
-const calc = document.querySelector('.calc');
-const age = document.querySelector('.age');
-const main = document.querySelector('.main');
